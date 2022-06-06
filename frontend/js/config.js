d3c9ee8862e1ad2debb3084213d0ea42c6d7afc5
Tebello Thejane <zyxoas@gmail.com>
2022-06-06 11:26:57 +0200

    Bare minimum frontend

---
+++ frontend/js/config.js
@@ -0,0 +1,3 @@
+const urlDomain = "http://localhost:8080";
+
+export {urlDomain};
--- frontend/js/scripts.js
+++ frontend/js/scripts.js
@@ -3,24 +3,43 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
 */
-// 
+//
 // Scripts
-// 
+//
+
+import { urlDomain } from "./config.js";
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-        // Uncomment Below to persist sidebar toggle between refreshes
-        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        //     document.body.classList.toggle('sb-sidenav-toggled');
-        // }
-        sidebarToggle.addEventListener('click', event => {
-            event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+    var names;
+    var justNames = [];
+
+    var urlD = urlDomain; //TODO: Why do we need to do this??
+
+    $.getJSON(urlD + '/api/allnames', function(data) {
+        names = data;
+
+        $.each(data, function(index, value) {
+            justNames.push(value.name.toUpperCase());
+            $('#poke-names').append("<option value='" + value.name + "''>");
         });
-    }
+    });
+
+    $("#poke-names-inp").on("input", function() {
+        if (justNames.includes($("#poke-names-inp")[0].value.toUpperCase())) {
+            var val = $("#poke-names-inp")[0].value;
+            var opts = $('#poke-names')[0].childNodes;
+            for (var i = 1; i < opts.length; i++) {
+                if (opts[i].value.toUpperCase() === val.toUpperCase()) {
+                    var name = opts[i].value;
+                    $("#heading")[0].innerText = name;
+                    $("#poke-img")[0].src = urlD + "/api/sprite/" + name.toLowerCase();
+                    $("#poke-img")[0].width = 500;
+                    $("#poke-img")[0].height = 500;
+                    break;
+                }
+            }
+        }
+    });
 
 });
